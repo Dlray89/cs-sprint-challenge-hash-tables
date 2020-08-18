40527f845c356e8a9fb6cb9c463cdeed6ae04394
Dlray89 <dlrayjr89@gmail.com>
2020-08-17 21:39:07 -0400

    file 1 isnt showing in push

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,25 @@
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    #find sum of two items(i, x) = limit
+    # return tuple
+    # if no pair return none
     # Your code here
+    item_dict = {}
+    for i in range(length):
+        if limit - weights[i] in item_dict:
+            key = limit - weights[i]
+            x = item_dict[key]
+            print('i', i)
+            print('x', x)
+            return [i, x]
+        item_dict[weights[i]] = i
+        print('item_dict', item_dict)
 
     return None
+
+
+weights = [4, 6, 10, 15, 16]
+get_indices_of_item_weights(weights, 5, 21)
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    values = {}
+    result = []
+
+    for i in a:
+        if abs(i) in values:
+            values[abs(i)] += 1
+            if values[abs(i)] > 1 and abs(i) not in result:
+                result.append(abs(i))
+            else:
+                values[abs(i)] = 1
 
     return result
 
